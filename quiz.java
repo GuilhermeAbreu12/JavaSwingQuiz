fcd354b380bc07b06af210d96afd0e7865ca5a1a
Guilherme Abreu <guilhermesabreu08@gmail.com>
2025-06-16 14:26:36 -0300

    Refactor: Parâmetros 'e' das classes True e FalseButton substituídos para resolver o problema de limpeza de código

--- quiz.java
+++ quiz.java
@@ -243,16 +243,15 @@ class Btn extends JButton{
     // Configurações para as alternativas corretas
     public static void TrueButton(JButton button){
         Styles.buttonStyle(button);
-        button.addActionListener(e-> {
+        button.addActionListener(_-> {
             button.setBackground(Cores.Verde01());
             System.out.println("[Evento] Usuário clicou no botão correto");
         });
     }
-
     // Configurações para as alternativas incorretas
     public static void FalseButton(JButton button){
         Styles.buttonStyle(button);
-        button.addActionListener(e -> {
+        button.addActionListener(_-> {
             button.setBackground(Color.RED);
             System.out.println("[Evento] Usuário clicou no botão errado");
         });
