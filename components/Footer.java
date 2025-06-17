2b9ed300ecc86a495a9367aba86ea9e296def0fd
Guilherme Abreu <guilhermesabreu08@gmail.com>
2025-06-17 18:58:57 -0300

    Style(Footer.java): Substitui a cor do background
    De azul padrão (Color.BLUE) para azul personalizado ( Cores.Azul01( ) )

--- components/Footer.java
+++ components/Footer.java
@@ -2,11 +2,13 @@ package components;
 import javax.swing.*;
 import java.awt.*;
 
+// Texto do footer
 class FooterText extends JLabel{
     public FooterText(){
-        // Texto do footer
+        // Texto (literalmente) do Footer
         super("<html><p>Desenvolvido por Guilherme S.Abreu, Heitor e João Pedro &copy; 2025</p></html>");
         
+        // Configurando o texto do Footer
         setFont(Fontes.Sans_serif_light());
         setForeground(Color.WHITE);
         setAlignmentX(Component.CENTER_ALIGNMENT);
@@ -14,11 +16,14 @@ class FooterText extends JLabel{
     }
 }
 
+// Criando o Footer
 public class Footer extends JPanel{
     public Footer() {
-        setBackground(Color.BLUE);
+        // Configurando o Footer
+        setBackground(Cores.Azul01());
         setPreferredSize(new Dimension(400, 60));
         setOpaque(true);
+        
         // Adicionando o FooterText
         add(new FooterText());
     }
