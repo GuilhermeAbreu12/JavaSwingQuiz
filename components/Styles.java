90d17d7068235ed937a37717709e1f6736e8a481
Guilherme Abreu <guilhermesabreu08@gmail.com>
2025-06-17 23:19:55 -0300

    Style(Styles): questionStyle com Layout Y_ASIS
    - Colocando Layout vertical no questionStyle
    - Cor Cinza01 removida, não estava surtindo efeito

--- components/Styles.java
+++ components/Styles.java
@@ -1,23 +1,23 @@
 package components;
 import java.awt.*;
-import javax.swing.JComponent;
-import javax.swing.JLabel;
-import javax.swing.JButton;
+import javax.swing.*;
 
 public class Styles {
     // Estilo dos retângulos de questões
     public static void questionStyle(JComponent component){
         Dimension size = new Dimension(900, 100);
+        component.setLayout(new BoxLayout(component, BoxLayout.Y_AXIS));
         component.setPreferredSize(size);
         component.setMaximumSize(size);
         component.setMinimumSize(size);
         component.setAlignmentX(Component.CENTER_ALIGNMENT);
-        component.setBackground(Cores.Cinza01());
     }
     // Estilo de texto padrão
     public static void fontStyle(JLabel label){
         label.setFont(Fontes.Sans_serif());
         label.setForeground(Color.BLACK);
+        label.setOpaque(false);
+        //label.setBackground(Cores.Cinza01());
         label.setAlignmentX(Component.CENTER_ALIGNMENT);
     }
     // Estilo dos botões
