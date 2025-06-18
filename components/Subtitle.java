f7392b5d1182c4d393354980ea30e996efdce0eb
Guilherme Abreu <guilhermesabreu08@gmail.com>
2025-06-17 23:16:01 -0300

    Style(Subtitle): Torna o JPanel subtítulo opaco

--- components/Subtitle.java
+++ components/Subtitle.java
@@ -1,11 +1,14 @@
 package components;
-
 import java.awt.*;
 import javax.swing.*;
 
+// Texto de subtítulo
 class SubtitleText extends JLabel {
     public SubtitleText(){
+        // Texto do subtítulo
         super("Teste seus conhecimentos sobre plurais");
+        
+        // Configurações do texto de subtítulo
         setFont(Fontes.Sans_serif());
         setForeground(Cores.Verde01());
         setAlignmentX(Component.CENTER_ALIGNMENT);
@@ -13,13 +16,17 @@ class SubtitleText extends JLabel {
     }
 }
 
+// Criando o subtítulo
 public class Subtitle extends JPanel{
     public Subtitle(){
         Dimension size = new Dimension(500, 40);
         setPreferredSize(size);
         setMaximumSize(size);
         setMinimumSize(size);
+        
+        setOpaque(false);
 
+        // Adicionando o texto
         add(new SubtitleText());
     }
 }
