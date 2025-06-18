b8efff0badcef23956cdda9abd79f765029a8f18
Guilherme Abreu <guilhermesabreu08@gmail.com>
2025-06-17 22:42:58 -0300

    Fix(Framer): Corrige a chamada do Main pela CustomScrollPane

--- components/Framer.java
+++ components/Framer.java
@@ -4,6 +4,7 @@ import javax.swing.JFrame;
 
 public class Framer extends JFrame{
     public Framer(){
+        // Configurações da janela
         setSize(1000,830);
         setLayout(new BorderLayout());
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -11,7 +12,7 @@ public class Framer extends JFrame{
         
         // Adicionando filhos
         add(new Header(), BorderLayout.NORTH);
-        add(new CustomScrollPane());
+        add(new CustomScrollPane(new Main()));
         add(new Footer(), BorderLayout.SOUTH);
 
         setVisible(true);
